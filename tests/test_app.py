b61c4fdcffca12cbcb4b7ec6a472dd4ac9728253
Robinson <robinson.lovatto@gmail.com>
2024-02-29 12:37:14 +0000

    refactoring of the code

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,19 +1,28 @@
 from selenium import webdriver
 from selenium.common.exceptions import TimeoutException
 from time import sleep
+import pytest
+import subprocess
 
-# define which driver is to use
-driver = webdriver.Firefox()
+# this decorator run for each test
+@pytest.fixture
+def driver():
+    # run streamlit in background
+    process = subprocess.Popen(["streamlit", "run", "src/app.py"])
 
-# define a timeout
-driver.set_page_load_timeout(5) # 5s
+    # define which driver is to use
+    driver = webdriver.Firefox()
 
-# try except to access the page
-try: 
+    # define a timeout
+    driver.set_page_load_timeout(5) # 5s
+
+    yield driver
+
+    # close webdriver and streamlit after the test
+    driver.quit()
+    process.kill()
+
+def test_app_open(driver):
+    # check if page open
     driver.get("http://localhost:8501")
     sleep(5)
-    print("Accessed the page succesfully")
-except TimeoutException:
-    print("Timeout")
-finally:
-    driver.quit()
